09344f86a10774003ec456ad99bff5c2922a9527
Krzysztof Nazar <72522808+KrzysztofNazar01@users.noreply.github.com>
2021-12-20 23:08:33 +0100

    Create suma_n_elementow_szer_kwad.c

---
+++ lab5/suma_n_elementow_szer_kwad.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+float suma_n_elementow_szer_kwad(int n);
+
+int main()
+{
+	int n = 3;
+	
+	float wynik = suma_n_elementow_szer_kwad(n);
+
+	printf("%f ", wynik);
+	
+
+	return 0;
+}
