aa5907b7e3d953b8d4ba30eae96d513c293615ec
Krzysztof Nazar <72522808+KrzysztofNazar01@users.noreply.github.com>
2022-02-02 16:20:57 +0100

    Update pomnoz_float.asm

--- kody_z_cwiczen/pomnoz_float.asm
+++ kody_z_cwiczen/pomnoz_float.asm
@@ -10,13 +10,9 @@ extern  _GetComputerNameW@8 : PROC
 
 _pomnoz_float PROC
 	mov eax, 01000000101000000000000000000000b ;liczba 5 zapisana binranie w formacie float (32 bity)
-	Mov ebx, eax
-	Shl ebx, 1 ;usun bit znaku
-	Shr ebx, 24 ;usun mantyse (23+1 = 24 bity)
-	Add ebx, 5 ;dodaj 5, czyli zwiększ wykładnik o 5, czyli pomnóż całą liczbę o 32
-	Shl ebx, 23 ;przywróć wykladnik na oryginalne miejsce
-	And eax, 807FFFFFh ;usun aktualny wykladnik (maska zer na 8 bitów odpowiadajacych za wykaldnik)
-	Or eax, ebx ;wstaw nowy wykladnik
+	ror eax, 23 
+	add eax, 5
+	rol eax, 23
 
 	;wynik--> eax = 5*32 = 160
 
